bdae717331e67c979119540de408aedbb684ac99
kazz <gang9696@gmail.com>
2023-11-24 22:20:39 +0900

    error edit

--- DeviceResources.cpp
+++ DeviceResources.cpp
@@ -104,6 +104,125 @@ void DeviceResources::CreateDeviceResources()
 #endif
 		}
 	}
+	if (m_options & c_EnableHDR) // 해당 플래그가 켜져 있는지 확인
+	{
+		ComPtr<IDXGIFactory5> factory5;
+		if (FAILED(m_dxgiFactory.As(&factory5)))
+		{
+			m_options &= ~c_EnableHDR;
+#ifdef _DEBUG
+			OutputDebugStringA("WARNING: HDR swap chains not supported");
+#endif
+		}
+	}
+	if (m_options & c_FlipPresent)
+	{
+		ComPtr<IDXGIFactory4> factory4;
+		if (FAILED(m_dxgiFactory.As(&factory4)))
+		{
+			m_options &= ~c_FlipPresent;
+#ifdef _DEBUG
+			OutputDebugStringA("INFO: Flip swap effects not supported");
+#endif
+		}
+	}
+	static const D3D_FEATURE_LEVEL s_featureLevels[] =
+	{
+		D3D_FEATURE_LEVEL_11_1,
+		D3D_FEATURE_LEVEL_11_0,
+		D3D_FEATURE_LEVEL_10_1,
+		D3D_FEATURE_LEVEL_10_0,
+		D3D_FEATURE_LEVEL_9_3,
+		D3D_FEATURE_LEVEL_9_2,
+		D3D_FEATURE_LEVEL_9_1
+	};
+
+	UINT featLevelCount = 0;
+	for (; featLevelCount < _countof(s_featureLevels); ++featLevelCount)
+	{
+		if (s_featureLevels[featLevelCount] < m_d3dMinFeatureLevel)
+			break;
+	}
+	if (!featLevelCount)
+	{
+		throw std::out_of_range("minFeatureLevel too high");
+	}
+	ComPtr<IDXGIAdapter1> adapter; 
+	GetHardwareAdapter(adapter.GetAddressOf()); 
+
+	ComPtr<ID3D11Device> device;
+	ComPtr<ID3D11DeviceContext> context;
+
+	HRESULT hr = E_FAIL;
+	if (adapter)
+	{
+		hr = D3D11CreateDevice(
+			adapter.Get(),
+			D3D_DRIVER_TYPE_UNKNOWN,
+			nullptr,
+			creationFlags,
+			s_featureLevels,
+			featLevelCount,
+			D3D11_SDK_VERSION,
+			device.GetAddressOf(),
+			&m_d3dFeatureLevel,
+			context.GetAddressOf()
+		);
+	}
+#if defined(NDEBUG)
+	else
+	{
+		throw std::exception("No Direct3D hardware device found");
+	}
+#else
+	if (FAILED(hr))
+	{
+		hr = D3D11CreateDevice(
+			nullptr,
+			D3D_DRIVER_TYPE_WARP,
+			nullptr,
+			creationFlags,
+			s_featureLevels,
+			featLevelCount,
+			D3D11_SDK_VERSION,
+			device.GetAddressOf(),
+			&m_d3dFeatureLevel,
+			context.GetAddressOf()
+		);
+
+		if (SUCCEEDED(hr))
+		{
+			OutputDebugStringA("Direct3D Adapter - WARP\n");
+		}
+	}
+#endif
+	ThrowIfFailed(hr);
+
+#ifdef NDEBUG
+	ComPtr<ID3D11Debug> d3dDebug;
+	if (SUCCEEDED(device.As(&d3dDebug)))
+	{
+		ComPtr<ID3D11InfoQueue> d3dInfoQueue;
+		if (SUCCEEDED(d3dDebug.As(&d3dInfoQueue)))
+		{
+#ifdef _DEBUG
+			d3dInfoQueue->SetBreakOnSeverity(D3D11_MESSAGE_SEVERITY_CORRUPTION, true);
+			d3dInfoQueue->SetBreakOnSeverity(D3D11_MESSAGE_SEVERITY_ERROR, true);
+#endif
+			D3D11_MESSAGE_ID hide[]=
+			{
+				D3D11_MESSAGE_ID_SETPRIVATEDATA_CHANGINGPARAMS,
+			};
+			D3D11_INFO_QUEUE_FILTER filter = {};
+			filter.DenyList.NumIDs = _countof(hide);
+			filter.DenyList.pIDList = hide;
+			d3dInfoQueue->AddStorageFilterEntries(&filter);
+		}
+	}
+#endif
+	ThrowIfFailed(device.As(&m_d3dDevice));
+	ThrowIfFailed(context.As(&m_d3dContext));
+	ThrowIfFailed(context.As(&m_d3dAnnotaion));
 }
 
 void DeviceResources::CreateWindowSizeDependentResources()
